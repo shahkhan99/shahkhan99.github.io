c7af2e79136c57c0b6c9e119170ac5a29a87ad40
shahkhan99 <hafizshahzaib2012@gmail.com>
2021-10-07 17:53:14 +0500

    deploy

---
+++ precache-manifest.695e9679f6e4f2030c0e1d7ca6516c6b.js
@@ -0,0 +1,378 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d596532f14541e5be55060a34a6be53f",
+    "url": "/shahkhan99.github.io/index.html"
+  },
+  {
+    "revision": "7c99af9c582473c6293d",
+    "url": "/shahkhan99.github.io/static/css/main.0677fa1b.chunk.css"
+  },
+  {
+    "revision": "47f29e6ce3be61ca8050",
+    "url": "/shahkhan99.github.io/static/js/2.8b71d69c.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/shahkhan99.github.io/static/js/2.8b71d69c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c99af9c582473c6293d",
+    "url": "/shahkhan99.github.io/static/js/main.b5dce846.chunk.js"
+  },
+  {
+    "revision": "aa52b8de38f4cee51b66",
+    "url": "/shahkhan99.github.io/static/js/runtime-main.13945669.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/shahkhan99.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/shahkhan99.github.io/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/shahkhan99.github.io/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/shahkhan99.github.io/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/shahkhan99.github.io/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/shahkhan99.github.io/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/shahkhan99.github.io/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/shahkhan99.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1851ccc81e939a9e49e1bb0fac40d05e",
+    "url": "/shahkhan99.github.io/static/media/WHlogo.1851ccc8.png"
+  },
+  {
+    "revision": "3b021f8ffcf9f323ea72abc28382e234",
+    "url": "/shahkhan99.github.io/static/media/address_image.3b021f8f.svg"
+  },
+  {
+    "revision": "49e0ee73ad51ac8da19f33c3d5c71fc2",
+    "url": "/shahkhan99.github.io/static/media/android-icon-144x144.49e0ee73.png"
+  },
+  {
+    "revision": "8fb6c26d3152020956b7b85edd388b81",
+    "url": "/shahkhan99.github.io/static/media/android-icon-192x192.8fb6c26d.png"
+  },
+  {
+    "revision": "b85a9e570e36cea6a13155c43343d080",
+    "url": "/shahkhan99.github.io/static/media/android-icon-96x96.b85a9e57.png"
+  },
+  {
+    "revision": "e5c4ba08c1a82ef1dc0c72885b61ca05",
+    "url": "/shahkhan99.github.io/static/media/android.e5c4ba08.svg"
+  },
+  {
+    "revision": "6c8410d0dc11fec239040695be13315d",
+    "url": "/shahkhan99.github.io/static/media/animated_ashutosh.6c8410d0.png"
+  },
+  {
+    "revision": "52e0714e5ea7e3b96f50dbfb62de21d1",
+    "url": "/shahkhan99.github.io/static/media/animated_logo.52e0714e.svg"
+  },
+  {
+    "revision": "516e5f02e09118172bd87fc4a9421f53",
+    "url": "/shahkhan99.github.io/static/media/apple-icon-114x114.516e5f02.png"
+  },
+  {
+    "revision": "c8ee763b0158d3d0edd48f915451627d",
+    "url": "/shahkhan99.github.io/static/media/apple-icon-120x120.c8ee763b.png"
+  },
+  {
+    "revision": "49e0ee73ad51ac8da19f33c3d5c71fc2",
+    "url": "/shahkhan99.github.io/static/media/apple-icon-144x144.49e0ee73.png"
+  },
+  {
+    "revision": "240a3f5c3ef6b3bcd3fdb5657d5b9e34",
+    "url": "/shahkhan99.github.io/static/media/apple-icon-152x152.240a3f5c.png"
+  },
+  {
+    "revision": "537f3c9241e642152bf3991d943065be",
+    "url": "/shahkhan99.github.io/static/media/apple-icon-180x180.537f3c92.png"
+  },
+  {
+    "revision": "29bfad611896e135ec14edc575ea9db0",
+    "url": "/shahkhan99.github.io/static/media/apple-icon-76x76.29bfad61.png"
+  },
+  {
+    "revision": "68e6e95dd09c30f53cdb6836af333330",
+    "url": "/shahkhan99.github.io/static/media/apple-icon-precomposed.68e6e95d.png"
+  },
+  {
+    "revision": "68e6e95dd09c30f53cdb6836af333330",
+    "url": "/shahkhan99.github.io/static/media/apple-icon.68e6e95d.png"
+  },
+  {
+    "revision": "a69e1b3418b00f8188170b69a69a148b",
+    "url": "/shahkhan99.github.io/static/media/aust.a69e1b34.png"
+  },
+  {
+    "revision": "ca462f63f90815d0b104f95c10b429c1",
+    "url": "/shahkhan99.github.io/static/media/aust_logo.ca462f63.png"
+  },
+  {
+    "revision": "b6b12c7d07e0569e771c8aedf81a7767",
+    "url": "/shahkhan99.github.io/static/media/blogs_image.b6b12c7d.svg"
+  },
+  {
+    "revision": "dfcb7a585547c0b125d208b980d6f57f",
+    "url": "/shahkhan99.github.io/static/media/cloud_infrastructure.dfcb7a58.svg"
+  },
+  {
+    "revision": "b3685be8bafcd5fe4786b39cba69a561",
+    "url": "/shahkhan99.github.io/static/media/codeInLogo.b3685be8.png"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/shahkhan99.github.io/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "434d2b012c3efc3da1ef90549a50f46b",
+    "url": "/shahkhan99.github.io/static/media/data_science.434d2b01.svg"
+  },
+  {
+    "revision": "7a9b2eb087350abacd2587dd578226c7",
+    "url": "/shahkhan99.github.io/static/media/deeplearning_ai_logo.7a9b2eb0.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/shahkhan99.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "f65ee6f64b46db0ae0f59279ea7853a8",
+    "url": "/shahkhan99.github.io/static/media/dsc_logo.f65ee6f6.png"
+  },
+  {
+    "revision": "caed892b2a0b817a8e275ae29638edaf",
+    "url": "/shahkhan99.github.io/static/media/education.caed892b.svg"
+  },
+  {
+    "revision": "85d6957f6a0d678d1ac3932e5be4fcb2",
+    "url": "/shahkhan99.github.io/static/media/experience.85d6957f.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/shahkhan99.github.io/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/shahkhan99.github.io/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/shahkhan99.github.io/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/shahkhan99.github.io/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/shahkhan99.github.io/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/shahkhan99.github.io/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/shahkhan99.github.io/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/shahkhan99.github.io/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/shahkhan99.github.io/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/shahkhan99.github.io/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/shahkhan99.github.io/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/shahkhan99.github.io/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/shahkhan99.github.io/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/shahkhan99.github.io/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/shahkhan99.github.io/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "b85a9e570e36cea6a13155c43343d080",
+    "url": "/shahkhan99.github.io/static/media/favicon-96x96.b85a9e57.png"
+  },
+  {
+    "revision": "a27467df5d535fee029a704916ad57f4",
+    "url": "/shahkhan99.github.io/static/media/favicon.a27467df.ico"
+  },
+  {
+    "revision": "2a24a8543299da438f46039d07384efa",
+    "url": "/shahkhan99.github.io/static/media/favicon1.2a24a854.ico"
+  },
+  {
+    "revision": "a73310ea0ad8aebedb65b3106024427a",
+    "url": "/shahkhan99.github.io/static/media/feelingProud.a73310ea.svg"
+  },
+  {
+    "revision": "589bec7d632ecff9672dc4da97897bbd",
+    "url": "/shahkhan99.github.io/static/media/fullstack.589bec7d.svg"
+  },
+  {
+    "revision": "ba18401b15f40a463898b706cc6ff08c",
+    "url": "/shahkhan99.github.io/static/media/gcp_logo.ba18401b.png"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "/shahkhan99.github.io/static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/shahkhan99.github.io/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/shahkhan99.github.io/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "6d325fec19fe3b6b169d9012f9a1dfb9",
+    "url": "/shahkhan99.github.io/static/media/ibm_logo.6d325fec.png"
+  },
+  {
+    "revision": "c350d7996596e9909fe72c0938cc9ee5",
+    "url": "/shahkhan99.github.io/static/media/iiitk_logo.c350d799.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/shahkhan99.github.io/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/shahkhan99.github.io/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "ed6b5c73e568abbcdeed63c01c4fc2a6",
+    "url": "/shahkhan99.github.io/static/media/kiet_logo.ed6b5c73.jpeg"
+  },
+  {
+    "revision": "4ba886bafb9ff0a5b71b800c47f45d90",
+    "url": "/shahkhan99.github.io/static/media/legato_logo.4ba886ba.png"
+  },
+  {
+    "revision": "260d57586012b0ed1ae78accc0bf7083",
+    "url": "/shahkhan99.github.io/static/media/logo512.260d5758.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/shahkhan99.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "fb870661a1a9fdac3ef16b12aac1f850",
+    "url": "/shahkhan99.github.io/static/media/michigan_logo.fb870661.png"
+  },
+  {
+    "revision": "dc75df84304aa6cbb377e7c37039bd41",
+    "url": "/shahkhan99.github.io/static/media/moon.dc75df84.svg"
+  },
+  {
+    "revision": "49e0ee73ad51ac8da19f33c3d5c71fc2",
+    "url": "/shahkhan99.github.io/static/media/ms-icon-144x144.49e0ee73.png"
+  },
+  {
+    "revision": "3fce30fff225db8d6a8a1247830d387f",
+    "url": "/shahkhan99.github.io/static/media/ms-icon-150x150.3fce30ff.png"
+  },
+  {
+    "revision": "6512b54b0ff4753ff98ce77b914dcd4e",
+    "url": "/shahkhan99.github.io/static/media/ms-icon-310x310.6512b54b.png"
+  },
+  {
+    "revision": "db6c1c0dddd4c868e00a3cffa1078e49",
+    "url": "/shahkhan99.github.io/static/media/muffito_logo.db6c1c0d.png"
+  },
+  {
+    "revision": "cd7077f7a08bffdb11e13093424e13fd",
+    "url": "/shahkhan99.github.io/static/media/nptel_logo.cd7077f7.png"
+  },
+  {
+    "revision": "6e7dc5ad476dd2d57600bca9521f3a5d",
+    "url": "/shahkhan99.github.io/static/media/portfolio.6e7dc5ad.gif"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/shahkhan99.github.io/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "f7964c6bf45e77547f1e5aae1e1dede0",
+    "url": "/shahkhan99.github.io/static/media/projects_image.f7964c6b.svg"
+  },
+  {
+    "revision": "bd5f4148a754968cb4444206539c565c",
+    "url": "/shahkhan99.github.io/static/media/pwa.bd5f4148.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/shahkhan99.github.io/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "da0e7c80b90df28dd3a590c80f3d4970",
+    "url": "/shahkhan99.github.io/static/media/stanford_logo.da0e7c80.png"
+  },
+  {
+    "revision": "4db24c0e662ca0fbacc25b3c959afedc",
+    "url": "/shahkhan99.github.io/static/media/sun.4db24c0e.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/shahkhan99.github.io/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/shahkhan99.github.io/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "ed3c02b43a5f2dfbd3a948f3e57364ed",
+    "url": "/shahkhan99.github.io/static/media/tasin.ed3c02b4.png"
+  },
+  {
+    "revision": "5720e0d78aab9577188fc3c56878ab5e",
+    "url": "/shahkhan99.github.io/static/media/tbd.5720e0d7.jpg"
+  },
+  {
+    "revision": "23e6a96a75a106f1d9b0280b7ff3ae75",
+    "url": "/shahkhan99.github.io/static/media/techglance.23e6a96a.png"
+  },
+  {
+    "revision": "67a6a11c47edafb87cf24da66afe3dca",
+    "url": "/shahkhan99.github.io/static/media/ui_ux_design.67a6a11c.svg"
+  }
+]);
